74b832d58fe4d25fb3ac5c4255e2009b1598d6f3
sgkim <107407274+georgek1205@users.noreply.github.com>
2023-06-14 16:30:19 +0900

    Create Test01.java

---
+++ String/Test01.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		//Q1
+		String first = "Oh ";
+		String second = "My ";
+		String third = "God ";
+		String combine = first + second + third;
+		System.out.println(combine);
+		
+		//Q2
+		System.out.print("Please input ID : ");
+		String id = scan.next();
+		System.out.println("Welcome! " + id);
+		
+    //Q3
+		scan.nextLine();
+		String[] combined = new String[3];
+		
+		for(int i = 0; i < 3; i++)
+		{
+			combined[i] = scan.next();
+			System.out.println(combined[i]);
+		}
+		
+	}
+
+}
